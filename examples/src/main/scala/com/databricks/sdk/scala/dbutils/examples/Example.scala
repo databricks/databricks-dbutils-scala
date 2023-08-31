f43d32f4307807ccadb06616a8de1fe2e6e71435
Miles Yucht <miles@databricks.com>
2023-09-01 00:54:24 +0200

    formatting

--- examples/src/main/scala/com/databricks/sdk/scala/dbutils/examples/Example.scala
+++ examples/src/main/scala/com/databricks/sdk/scala/dbutils/examples/Example.scala
@@ -3,8 +3,8 @@ package com.databricks.sdk.scala.dbutils.examples
 import com.databricks.sdk.scala.dbutils.DBUtils
 
 object Example {
-    final def main(args: Array[String]): Unit = {
-        val dbutils = DBUtils.getDBUtils()
+  final def main(args: Array[String]): Unit = {
+    val dbutils = DBUtils.getDBUtils()
 
-    }
+  }
 }
