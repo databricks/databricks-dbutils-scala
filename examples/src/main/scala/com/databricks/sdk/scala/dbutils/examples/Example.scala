5904df7fb5a472a3c639bf60f526b788a374d31c
Miles Yucht <miles@databricks.com>
2023-09-01 00:40:16 +0200

    format

---
+++ examples/src/main/scala/com/databricks/sdk/scala/dbutils/examples/Example.scala
@@ -0,0 +1,10 @@
+package com.databricks.sdk.scala.dbutils.examples
+
+import com.databricks.sdk.scala.dbutils.DBUtils
+
+object Example {
+    final def main(args: Array[String]): Unit = {
+        val dbutils = DBUtils.getDBUtils()
+
+    }
+}
